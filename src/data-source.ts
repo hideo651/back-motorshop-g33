26a7ff3ec76b53778f7882564540849a19e0befb
douglasB834 <douglas_borges09@hotmail.com>
2023-04-03 16:00:27 -0300

    feat: add more components entity

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,18 @@
 import { DataSource } from "typeorm";
 import path from "path";
 import "dotenv/config";
+import { createUsersTable1671032457673 } from "./migrations/1671032457673-createUsersTable";
+import { User } from "./entities/user.entity";
 
 const AppDataSource = new DataSource(
-    process.env.NODE_ENV === "test" ?
-    {
+  process.env.NODE_ENV === "test"
+    ? {
         type: "sqlite",
         database: ":memory:",
         synchronize: true,
-        entities: ["src/entities/*.ts"]
-    } :
-    {
+        entities: ["src/entities/*.ts"],
+      }
+    : {
         type: "postgres",
         host: process.env.PGHOST,
         port: parseInt(process.env.PGPORT!),
@@ -19,9 +21,9 @@ const AppDataSource = new DataSource(
         database: process.env.PGDATABASE,
         logging: true,
         synchronize: false,
-        entities: [path.join(__dirname, "./entities/**.{js,ts}")],
-        migrations: [path.join(__dirname, "./migrations/**.{js,ts}")]
-    }
-)
+        entities: [User],
+        migrations: [createUsersTable1671032457673],
+      }
+);
 
 export default AppDataSource;
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,35 +1,69 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, BeforeInsert, BeforeUpdate } from "typeorm";
-import { hashSync } from "bcrypt";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
-@Entity('users')
+@Entity("users")
 class User {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @Column({ length:100 })
-    name: string;
+  @Column({ length: 100 })
+  name: string;
 
-    @Column({ length:100, unique: true })
-    email: string;
+  @Column({ length: 100, unique: true })
+  email: string;
 
-    @Column({length: 120,})
-    password: string;
+  @Column({ length: 100, unique: true })
+  cpf: string;
 
-    @Column({default: false})
-    isAdm: boolean;
+  @Column({ length: 100 })
+  cellphone: string;
 
-    @Column({default: true})
-    isActive: boolean;
+  @Column()
+  birthday: Date;
 
-    @CreateDateColumn()
-    createdAt: Date;
+  @Column({ length: 200 })
+  description: string;
 
-    @UpdateDateColumn()
-    updatedAt: Date;
+  @Column({ length: 20 })
+  cep: string;
 
-    @BeforeInsert()
-    @BeforeUpdate()
-    hashPassword(){this.password = hashSync(this.password, 10)};
+  @Column({ length: 120 })
+  state: string;
+
+  @Column({ length: 120 })
+  city: string;
+
+  @Column({ length: 120 })
+  street: string;
+
+  @Column({ length: 120 })
+  number: string;
+
+  @Column({ nullable: true })
+  complemente: string;
+
+  @Column({ default: false })
+  isStaff: boolean;
+
+  @Column({ length: 120 })
+  password: string;
+
+  @Column({ default: false })
+  isAdm: boolean;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
 
-export {User};
+export { User };
