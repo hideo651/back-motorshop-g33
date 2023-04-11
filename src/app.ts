0cd088b91de0555ac5fbaae5ad77c13ce1975da8
douglasB834 <douglas_borges09@hotmail.com>
2023-04-11 11:01:30 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,12 @@ import userRoutes from "./routers/user.routes";
 import loginRoutes from "./routers/login.routes";
 import handleError from "./errors/HandleError";
 import announcementRoutes from "./routers/announcement.routes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/users", userRoutes);
 app.use("/login", loginRoutes);
 app.use("/announcement", announcementRoutes);
