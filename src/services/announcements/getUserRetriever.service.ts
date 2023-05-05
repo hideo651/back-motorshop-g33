098f9257ac238de8002b6ca471a8171fc7b73c8f
douglasB834 <douglas_borges09@hotmail.com>
2023-05-05 17:45:37 -0300

    feat:add route updade comment

--- src/services/announcements/getUserRetriever.service.ts
+++ src/services/announcements/getUserRetriever.service.ts
@@ -13,6 +13,7 @@ export const getUserRetrieverService = async (
     .select([
       "comment",
       "comment_user.name",
+      "comment_user.id",
       "announcement",
       "photos",
       "user.id",
